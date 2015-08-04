21df757bd74d4d01d8d21f865a3cce5f835aec84
PetyoKostakov <petyokostakov@gmail.com>
2015-08-04 16:24:45 +0300

    Gulp support

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Petyo on 8/3/2015.
+ */
+var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+
+gulp.task('default', function () {
+    nodemon({
+        script: 'server.js',
+        ext: 'js',
+        env: {
+            PORT: 8000
+        },
+        ignore: ['./node_modules/**']
+    }).on('restart', function () {
+        console.log('Restarting');
+    })
+});
